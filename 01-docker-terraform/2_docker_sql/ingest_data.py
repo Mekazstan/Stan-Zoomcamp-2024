d696d030c45bb5be9aff3c4a3e14a58a503f6cfc
Chukwuemeka <mekastans@gmail.com>
2024-02-02 11:21:40 +0100

    modify injest_data script

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -14,14 +14,16 @@ def main(params):
     port = params.port 
     db = params.db
     table_name = params.table_name
-    url = params.url
+    # url = params.url
     
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
+    # if url.endswith('.csv.gz'):
+    #     csv_name = 'output.csv.gz'
+    # else:
+    #     csv_name = 'output.csv'
 
-    os.system(f"wget {url} -O {csv_name}")
+    # os.system(f"wget {url} -O {csv_name}")
+    
+    csv_name = 'output.csv'
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
@@ -66,7 +68,7 @@ if __name__ == '__main__':
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    # parser.add_argument('--url', required=True, help='url of the csv file')
 
     args = parser.parse_args()
 
