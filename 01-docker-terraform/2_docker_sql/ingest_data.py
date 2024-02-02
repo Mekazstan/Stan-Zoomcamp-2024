efb22f806fd5da95d00a7dc9fbdbdfe7f386081e
Chukwuemeka <mekastans@gmail.com>
2024-02-02 14:03:10 +0100

    modified ingestion script

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -83,13 +83,25 @@ from time import time
 import pandas as pd
 from sqlalchemy import create_engine
 
-def main(params):
-    user = params.user
-    password = params.password
-    container_id_or_name = params.container_id_or_name
-    port = params.port 
-    db = params.db
-    table_name = params.table_name
+def main():
+    
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--container_id_or_name', required=True, help='container ID or name for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+
+    args = parser.parse_args()
+    
+    user = args.user
+    password = args.password
+    container_id_or_name = args.container_id_or_name
+    port = args.port 
+    db = args.db
+    table_name = args.table_name
 
     csv_name = 'yellow_tripdata_2021-01.csv'
 
@@ -127,18 +139,7 @@ def main(params):
             break
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--container_id_or_name', required=True, help='container ID or name for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-
-    args = parser.parse_args()
-
-    main(args)
+    main()
     
     
 # python3 ingest_data.py --user root --password root --container_id_or_name e66de0f0aa8c --port 5432 --db ny_taxi --table_name yellow_taxi_data
