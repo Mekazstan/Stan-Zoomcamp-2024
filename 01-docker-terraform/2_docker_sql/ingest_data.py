5f918c5fc6345e3c65859c7363a1bd2965cad735
Chukwuemeka <mekastans@gmail.com>
2024-02-02 13:26:19 +0100

    modified ingestion script

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -1,3 +1,80 @@
+# import os
+# import argparse
+
+# from time import time
+
+# import pandas as pd
+# from sqlalchemy import create_engine
+
+
+# def main(params):
+#     user = params.user
+#     password = params.password
+#     host = params.host 
+#     port = params.port 
+#     db = params.db
+#     table_name = params.table_name
+#     # url = params.url
+    
+#     # if url.endswith('.csv.gz'):
+#     #     csv_name = 'output.csv.gz'
+#     # else:
+#     #     csv_name = 'output.csv'
+
+#     # os.system(f"wget {url} -O {csv_name}")
+    
+#     csv_name = 'yellow_tripdata_2021-01.csv'
+
+#     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+#     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+#     df = next(df_iter)
+
+#     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+#     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+#     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+#     df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+#     while True: 
+
+#         try:
+#             t_start = time()
+            
+#             df = next(df_iter)
+
+#             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+#             df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+#             df.to_sql(name=table_name, con=engine, if_exists='append')
+
+#             t_end = time()
+
+#             print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+#         except StopIteration:
+#             print("Finished ingesting data into the postgres database")
+#             break
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+#     parser.add_argument('--user', required=True, help='user name for postgres')
+#     parser.add_argument('--password', required=True, help='password for postgres')
+#     parser.add_argument('--host', required=True, help='host for postgres')
+#     parser.add_argument('--port', required=True, help='port for postgres')
+#     parser.add_argument('--db', required=True, help='database name for postgres')
+#     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+#     # parser.add_argument('--url', required=True, help='url of the csv file')
+
+#     args = parser.parse_args()
+
+#     main(args)
+
+
 import os
 import argparse
 
@@ -6,26 +83,17 @@ from time import time
 import pandas as pd
 from sqlalchemy import create_engine
 
-
 def main(params):
     user = params.user
     password = params.password
-    host = params.host 
+    container_id_or_name = params.container_id_or_name
     port = params.port 
     db = params.db
     table_name = params.table_name
-    # url = params.url
-    
-    # if url.endswith('.csv.gz'):
-    #     csv_name = 'output.csv.gz'
-    # else:
-    #     csv_name = 'output.csv'
 
-    # os.system(f"wget {url} -O {csv_name}")
-    
     csv_name = 'yellow_tripdata_2021-01.csv'
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine = create_engine(f'postgresql://{user}:{password}@{container_id_or_name}:{port}/{db}')
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
@@ -38,12 +106,9 @@ def main(params):
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-
     while True: 
-
         try:
             t_start = time()
-            
             df = next(df_iter)
 
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
@@ -64,15 +129,14 @@ if __name__ == '__main__':
 
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--container_id_or_name', required=True, help='container ID or name for postgres')
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    # parser.add_argument('--url', required=True, help='url of the csv file')
 
     args = parser.parse_args()
 
     main(args)
-
-
-# python3 ingest_data.py --user root --password root --host localhost --port 5432 --db ny_taxi --table_name yellow_taxi_data
+    
+    
+# python3 ingest_data.py --user root --password root --container_id_or_name e66de0f0aa8c --port 5432 --db ny_taxi --table_name yellow_taxi_data
